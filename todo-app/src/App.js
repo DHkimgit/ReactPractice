5886d3ef778f15ff4f332843119eedbf2d67aa03
DHkimgit <dukekim001@naver.com>
2022-06-25 07:19:15 +0000

    2206251619

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,31 +1,56 @@
-import React, {usestate} from 'react'
+import React, {useState, useRef, useCallback} from 'react'
 import './App.css';
 import TodoTnsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 import TodoTemplate from './components/TodoTemplate';
 
+function createBulkTodos() {
+  const array = [];
+  for (let i = 0; i<=2500; i++) {
+    array.push({
+      id:i,
+      text: `할일 ${i}`,
+      chseked: false,
+    });
+  }
+  return array;
+}
+
 const App = () => {
-  const [todos, setTodos] = usestate([
-    {
-      id: 1,
-      text: '리액트의 기초 알아보기',
-      checked: true,
+  const [todos, setTodos] = useState(createBulkTodos);
+  const onRemove = useCallback(
+    id => {
+      setTodos(todos => todos.filter(todo => todo.id !== id));
     },
-    {
-      id: 2,
-      text: '컴포넌트 스타일링 해보기',
-      checked: true,
+    [],
+  );
+  const onToggle = useCallback(
+    id => {
+      setTodos(todos =>
+        todos.map(todo => 
+          todo.id === id ? {...todo, checked: !todo.checked} : todo,
+          ),
+      );
     },
-    {
-      id: 3,
-      text: '일정 관리 앱 만들어 보기',
-      checked: false,
-    },
-  ]);
+    [],
+  );
+  //고윳값으로 사용될 id
+  //ref를 사용하여 변수 담기
+  const nextId = useRef(2501);
+  const onInsert = useCallback(
+    text => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+      setTodos(todos => todos.concat(todo));
+      nextId.current += 1;
+    }, [],);
   return (
     <TodoTemplate>
-      <TodoTnsert />
-      <TodoList todos={todos}/>
+      <TodoTnsert onInsert={onInsert}/>
+      <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
     </TodoTemplate>
   )
 
